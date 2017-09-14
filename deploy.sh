456608ad7659e82eaf06688d85cfdeeb6f777520
Julien Vincent <julienlucvincent@gmail.com>
2017-09-14 22:42:32 +0200

    fix: deploy.sh not properly checking namespace

--- deploy.sh
+++ deploy.sh
@@ -3,24 +3,20 @@ export STAGE="$1"
 export NAMESPACE="$2"
 export IMAGE_NAME=eu.gcr.io/one-day-only-infrastructure/redis-${STAGE}:$(uuidgen)
 
-if [ -z "${STAGE}" ]
-then {
-    echo No stage defined
-    exit 1
-  }
+if [[ -n ${STAGE} ]]; then
+  echo "--> No stage provided. Please provide a stage as first argument"
+  exit 1
 fi
 
-if [ -z "${STAGE}" ]
-then {
-    NAMESPACE=default
-  }
+if [[ -n ${NAMESPACE} ]]; then
+  NAMESPACE=default
 fi
 
 docker build . -t ${IMAGE_NAME}
 
-if gcloud docker -- push ${IMAGE_NAME}
-then echo "container pushed correctly"
-else echo "error pushing container to registry" && exit 1
+if ! gcloud docker -- push ${IMAGE_NAME}; then
+  echo "--> Error pushing container to registry"
+  exit 1
 fi
 
 mkdir _redis
@@ -28,7 +24,7 @@ mkdir _redis
 envsubst < deployment/Namespace.yml > _redis/00Namespace.yml
 envsubst < deployment/Services.yml > _redis/10Services.yml
 envsubst < deployment/Sentinel.yml > _redis/20Sentinel.yml
-envsubst < deployment/Slave.yml > _redis/Slave.yml
+envsubst < deployment/Slave.yml > _redis/30Slave.yml
 
 kubectl apply -f ./_redis/ --record
 
