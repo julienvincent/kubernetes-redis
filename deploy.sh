1327d34296727c120465a9e3b21e6b5436bd4dd4
Julien Vincent <julienlucvincent@gmail.com>
2017-09-14 22:50:47 +0200

    registry check invalid

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,19 @@
 # #!/usr/bin/env bash
 export STAGE=${1}
-export REGISTRY=${2}
+REGISTRY=${2}
 export NAMESPACE=${3}
-
-if [[ ! -n ${REGISTRY }]]; then
-  echo "--> No registry provided. Please provide a google registry as the second argument"
-fi
-
-export IMAGE_NAME=eu.gcr.io/${REGISTRY}/redis-${STAGE}:$(uuidgen)
 
 if [[ ! -n ${STAGE} ]]; then
   echo "--> No stage provided. Please provide a stage as first argument"
   exit 1
 fi
+
+if [[ ! -n ${REGISTRY} ]]; then
+  echo "--> No registry provided. Please provide a google registry as the second argument"
+  exit 1
+fi
+
+export IMAGE_NAME=eu.gcr.io/${REGISTRY}/redis-${STAGE}:$(uuidgen)
 
 if [[ ! -n ${NAMESPACE} ]]; then
   NAMESPACE=default
